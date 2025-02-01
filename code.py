676dd798c6a2dd3a676314104425ff1b2e692cb4
Syed Faizaan Mustafa <100984813+FaizaanMustafa@users.noreply.github.com>
2025-02-01 11:32:32 -0800

    Update code.py

--- code.py
+++ code.py
@@ -4,3 +4,5 @@ import pandas as pd
 orders_df = spark.read.format("csv").option('inderschema',True).load('/public/data/hello')
 
 customers_df = spark.read.format("csv").option('inderschema',True).load('/public/data/hello')
+
+my data is love
