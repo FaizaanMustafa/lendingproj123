6d65f3414a1f3d31b6a7d4622efb5508d0cccebc
Faizaan <faizaanmustafa786@gmail.com>
2025-02-01 11:22:46 -0800

    added new files

--- code.py
+++ code.py
@@ -1,1 +1,18 @@
 import pandas as pd
+
+import pandas as def df(x):
+   return x*x
+
+#df is your dataframe
+#example function is applicable for all INT dataframe 
+df_squared = df.apply(yourFunction)(x):
+   return x*x
+
+#df is your dataframe
+#example function is applicable for all INT dataframe 
+df_squared = df.apdef yourFunction(x):
+   return x*x
+
+#df is your dataframe
+#example function is applicable for all INT dataframe 
+df_squared = df.apply(yourFunction))
